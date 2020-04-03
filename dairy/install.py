deefc766775615bdcba02ac1b3a8ac982fb3e7f2
smehata <smehata@dexciss.com>
2020-04-03 17:04:56 +0530

    updated rmrd in nextdairy

--- dairy/install.py
+++ dairy/install.py
@@ -2,5 +2,7 @@ from __future__ import print_function, unicode_literals
 import frappe
 
 def after_install():
+    print("@@@@@@@@@@@@@@@@@@@@2------------------@@@@@@@@@@@@@@@@@")
     res = frappe.db.sql("""INSERT INTO `tabDomain` (domain) VALUES ('Dairy')""")
+    print("@@@@@@@@@@@@@@@@@@@@2------------------@@@@@@@@@@@@@@@@@",res)
     res.db_update()
