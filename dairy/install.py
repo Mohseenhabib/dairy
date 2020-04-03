9cb75933b441d9f23ddfd7285b64aa445faf0fe9
smehata <smehata@dexciss.com>
2020-04-03 16:38:20 +0530

    updated rmrd in nextdairy

--- dairy/install.py
+++ dairy/install.py
@@ -2,5 +2,5 @@ from __future__ import print_function, unicode_literals
 import frappe
 
 def after_install():
-    res = frappe.db.sql("""INSERT INTO `tabTender Category` (domain) VALUES ('Dairy')""")
+    res = frappe.db.sql("""INSERT INTO `tabDomain` (domain) VALUES ('Dairy')""")
     res.db_update()
