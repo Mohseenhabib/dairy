17d316ce51df00404e32c96a1af7e1033c5d461f
smehata <smehata@dexciss.com>
2020-04-03 18:24:01 +0530

    updated nextdairy

--- dairy/install.py
+++ dairy/install.py
@@ -2,7 +2,5 @@ from __future__ import print_function, unicode_literals
 import frappe
 
 def after_install():
-    print("@@@@@@@@@@@@@@@@@@@@2------------------@@@@@@@@@@@@@@@@@")
     res = frappe.db.sql("""INSERT INTO `tabDomain` (name,domain) VALUES ('Dairy','Dairy')""")
-    print("@@@@@@@@@@@@@@@@@@@@2------------------@@@@@@@@@@@@@@@@@",res)
     res.db_update()
