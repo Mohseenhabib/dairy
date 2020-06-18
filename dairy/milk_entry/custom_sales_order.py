f9dc0e5d2c7a2aff49977c7f0ee8acbc99ad7c3b
smehata <smehata@dexciss.com>
2020-06-18 12:16:29 +0530

    updated leakage quantity functionality

--- dairy/milk_entry/custom_sales_order.py
+++ dairy/milk_entry/custom_sales_order.py
@@ -5,23 +5,41 @@ def before_submit(sales, method):
     if frappe.db.get_single_value("Dairy Settings", "leakage_percentage") and frappe.db.get_single_value("Dairy Settings", "leakage_qty"):
         leakage_perc = float(frappe.db.get_single_value("Dairy Settings", "leakage_percentage"))
         leakage_qty = float(frappe.db.get_single_value("Dairy Settings", "leakage_qty"))
+        applicable_on = (frappe.db.get_single_value("Dairy Settings", "applicable_on"))
         lst = []
         for line in sales.items:
             lst.append(line)
         for line in lst:
             item = frappe.get_doc("Item",line.item_code)
-            if item.leakage_applicable and line.stock_qty > leakage_qty:
+            if item.leakage_applicable and applicable_on == "Stock UOM" and line.stock_qty > leakage_qty :
                 sales.append("items",{
                     "item_code": line.item_code,
                     "item_name": line.item_name,
                     "delivery_date": line.delivery_date,
-                    "description": line.description,
+                    "description": str(line.description)+" Leakage Scheme applied",
+                    "gst_hsn_code": line.gst_hsn_code,
+                    "is_nil_exempt": line.is_nil_exempt,
+                    "qty": float(((str((line.stock_qty * leakage_perc)/100)+".").split("."))[0]),
+                    "uom": line.stock_uom,
+                    "stock_uom": line.stock_uom,
+                    # "conversion_factor": line.conversion_factor,
+                    "rate": line.rate,
+                    "warehouse": line.warehouse,
+                })
+                sales.validate()
+
+            if item.leakage_applicable and applicable_on == "Order UOM" and line.qty > leakage_qty :
+                sales.append("items",{
+                    "item_code": line.item_code,
+                    "item_name": line.item_name,
+                    "delivery_date": line.delivery_date,
+                    "description": str(line.description)+" Leakage Scheme applied",
                     "gst_hsn_code": line.gst_hsn_code,
                     "is_nil_exempt": line.is_nil_exempt,
                     "qty": float(((str((line.qty * leakage_perc)/100)+".").split("."))[0]),
                     "uom": line.uom,
                     "stock_uom": line.stock_uom,
-                    "conversion_factor": line.conversion_factor,
+                    # "conversion_factor": line.conversion_factor,
                     "rate": line.rate,
                     "warehouse": line.warehouse,
                 })
