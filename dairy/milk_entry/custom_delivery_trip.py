2b33817df813a7c2951df5a098efbb3f7ec2e5cc
Navnath Gavde <gavdenavnath.44@gmail.com>
2020-10-19 17:10:26 +0530

    gate pass details update

--- dairy/milk_entry/custom_delivery_trip.py
+++ dairy/milk_entry/custom_delivery_trip.py
@@ -44,7 +44,7 @@ def get_jinja_data_del_note_item(del_note):
 	right outer Join `tabCrate Count Child` B
 	on A.item_code = B.item_code
 	where 
-		A.parent = %(name)s and B.parent = %(name)s """, {"name": del_note}, as_dict=True)
+		A.parent = %(name)s and B.parent = %(name)s and A.is_free_item = 0 """, {"name": del_note}, as_dict=True)
 
 	dist_itm = frappe.db.sql(""" select distinct(item_code) from `tabDelivery Note Item` where parent = %(name)s """,
 							 {'name':del_note})
