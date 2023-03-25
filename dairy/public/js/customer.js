a9a3e06d4d9cf955219f3a888c4ac1bbf46c23a2
Shweta <schoudhary@dexciss.com>
2023-03-25 11:09:45 +0530

    customer : cancelled filter

--- dairy/public/js/customer.js
+++ dairy/public/js/customer.js
@@ -13,4 +13,35 @@ frappe.ui.form.on("Customer", {
             };
         });
     },
+    refresh: function(frm,cdt,cdn){
+        frm.fields_dict['links'].grid.get_field('link_name').get_query = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn]
+            return {    
+                filters:[
+                    ['docstatus', '!=', 2]
+                   
+                ]
+            }
+
+        }
+    }
 });
+// frappe.ui.form.on('Dynamic Link', {
+//     link_doctype:function(frm,cdt,cdn){
+//         console.log('%%%%%%%%%%%%%%%%')
+//         var child = locals[cdt][cdn]
+//         if(child.link_doctype){
+//             var sub = 0;
+//             frappe.db.get_doc('DocType',child.link_doctype).then(ld => {
+//                 if (ld.is_submittable == 1){
+//                     var sub = 1
+//                 }
+//             })
+//             if(sub == 1){
+            
+//         }
+            
+//         }
+//     }
+    
+// });
