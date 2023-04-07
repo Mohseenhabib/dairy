cb2ce83a08f3722677392c9054070958c5302878
Shweta <schoudhary@dexciss.com>
2023-04-07 11:47:45 +0530

    purchase receipt

--- dairy/public/js/purchase_receipt.js
+++ dairy/public/js/purchase_receipt.js
@@ -33,7 +33,7 @@ frappe.ui.form.on('Purchase Receipt Item', {
         }
 	},
 
-    snf_clr_per: function(frm, cdt, cdn) {
+   snf_clr_per: function(frm, cdt, cdn) {
 	    var d = locals[cdt][cdn];
         if(d.snf_clr_per){
            var weight = d.total_weight;
@@ -41,5 +41,56 @@ frappe.ui.form.on('Purchase Receipt Item', {
            frappe.model.set_value(cdt, cdn, "clr", snf_clr);
         }
 	},
+
+   clr_per: function(frm, cdt, cdn) {
+      var d = locals[cdt][cdn];
+      if(d.clr_per){
+         var weight = d.total_weight;
+         var snf = ((d.clr_per / 100) * weight);
+         frappe.model.set_value(cdt, cdn, "snf", snf);
+      }
+  },
+
+   snf: function(frm, cdt, cdn) {
+      var d = locals[cdt][cdn];
+      if(d.snf){
+         var weight = d.total_weight;
+         var per = ((d.snf / weight) * 100);
+         frappe.model.set_value(cdt, cdn, "clr_per", per);
+      }
+   },
+
+
+   qty : function(frm, cdt, cdn){
+      var d = locals[cdt][cdn];
+      if(d.qty){
+         frappe.db.get_value("Item", d.item_code, ["weight_per_unit"]).then((r) => {
+               console.log('rrrrrrrrrrrrrrrrrrrrr',r.message,d.qty)
+         
+         if(d.clr_per){
+            var weight = d.total_weight;
+            var snf = ((d.clr_per / 100) * weight);
+            console.log('snfFFFFFFFFFFF',weight,d.clr_per)
+            frappe.model.set_value(cdt, cdn, "snf", snf);
+         }
+         if(d.snf_clr_per){
+            var weight = d.total_weight;
+            console.log('snf_clrRRRRRRRRRRRRRRRRRRR',weight,d.snf_clr_per)
+            var snf_clr = ((d.snf_clr_per / 100) * weight);
+            frappe.model.set_value(cdt, cdn, "clr", snf_clr);
+         }
+         if(d.fat_per_){
+            var weight = d.total_weight;
+            var fat = ((d.fat_per_ / 100) * weight);
+            console.log('fatTTTTTTTTTTTTTTTTTTTTTttt',weight,d.fat_per_)
+            frappe.model.set_value(cdt, cdn, "fat", fat);
+         }
+
+        })
+      }
+   },
+
+
+  
 
 });
