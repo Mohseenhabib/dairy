238ae421fcc87a65a999f3c66213bc4c4f84f9eb
Shubhamk-10 <shubhamkuthe5@gmail.com>
2023-04-14 15:43:02 +0530

    feat: new custom files for payment entry

---
+++ dairy/public/js/custom_payment_entry_list.js
@@ -0,0 +1,12 @@
+frappe.listview_settings['Payment Entry'] = {
+    onload: function(list_view) {
+		list_view.page.add_inner_button(__("Download CSV"), function() { 
+        
+        frappe.call({
+            method: "dairy.milk_entry.custom_payment_entry.get_filter_data",
+            args: {filter_list: list_view.filters}
+
+            })
+        })
+	}
+}
