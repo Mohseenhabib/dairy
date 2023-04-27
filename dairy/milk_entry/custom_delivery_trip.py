0965488a3134f61bff2eb522be8848fa7c5e99dd
Ajay <ajayjogdand374@gmail.com>
2023-04-27 19:09:02 +0530

    fix:print format

--- dairy/milk_entry/custom_delivery_trip.py
+++ dairy/milk_entry/custom_delivery_trip.py
@@ -187,10 +187,10 @@ def get_purchase(pr):
 	doc=frappe.get_doc("Purchase Invoice",pr)
 	dlst=[]
 	for j in doc.items:
-		k={}
+		h={}
 		pr_item=frappe.get_doc("Purchase Receipt",j.purchase_receipt)
 		for k in pr_item.items: 
-			k.update({"ltr":k.qty,"fat":k.fat_per_,"snf":k.snf_clr_per,"rate":k.rate,"amount":k.amount,"posting_date":format_date(pr_item.posting_date),"shift":pr_item.shift})
+			h.update({"ltr":k.qty,"fat":k.fat_per_,"snf":k.snf_clr_per,"rate":k.rate,"amount":k.amount,"posting_date":format_date(pr_item.posting_date),"shift":pr_item.shift})
 		dlst.append(k)
 	sorted_data = sorted(dlst, key=lambda x: (x["posting_date"], x["shift"].lower() != "morning"))
 
