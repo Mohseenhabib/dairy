dd20b622a58b3ff4d9cee637577dba1915d4a3a4
Shweta <schoudhary@dexciss.com>
2023-05-17 12:14:50 +0530

    van collection

--- dairy/milk_entry/doctype/van_collection/van_collection.py
+++ dairy/milk_entry/doctype/van_collection/van_collection.py
@@ -346,6 +346,18 @@ class VanCollection(Document):
                                 'sample_lines': res.get('name')
                             })     
                    
+
+                    doc=frappe.get_doc("Dairy Settings")
+                    item=0.0
+                    if i.get("milk_type")=="Cow":
+                        item = frappe.db.get_value('Item',{"name":doc.cow_pro},['weight_per_unit'])
+                        # print('cow item______________________',item)
+                    elif i.get("milk_type")=="Buffalo":
+                        item = frappe.db.get_value('Item',{"name":doc.buf_pro},['weight_per_unit'])
+                        # print('buffalo item ______________________________',item)
+                    elif i.get("milk_type")=="Mix":
+                        item = frappe.db.get_value('Item',{"name":doc.mix_pro},['weight_per_unit'])
+                        # print('mix item****************************',item)
                     # print('cow_volume*********************************',cow_volume,cow_milk_fatin_kg,item)
                     if flt(cow_volume) > 0:
                         # print('cow_volume*********************************',cow_volume,cow_milk_fatin_kg,item)
