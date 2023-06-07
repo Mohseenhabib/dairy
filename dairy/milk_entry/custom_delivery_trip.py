5cb7dba564781849c878b6c15575d51589498ebc
Shweta <schoudhary@dexciss.com>
2023-06-07 17:51:45 +0530

    farmer bill:Remove deduction

--- dairy/milk_entry/custom_delivery_trip.py
+++ dairy/milk_entry/custom_delivery_trip.py
@@ -190,8 +190,9 @@ def get_purchase(pr):
 		h={}
 		pr_item=frappe.get_doc("Purchase Receipt",j.purchase_receipt)
 		milk = frappe.get_doc('Milk Entry',pr_item.milk_entry)
+		a = (milk.unit_price) - (milk.snf_deduction_per)
 		for k in pr_item.items: 
-			h.update({"ltr":k.qty,"fat":k.fat_per_,"snf":k.snf_clr_per,"rate":milk.unit_price,"amount":k.amount,"posting_date":format_date(pr_item.posting_date),"shift":pr_item.shift})
+			h.update({"ltr":k.qty,"fat":k.fat_per_,"snf":k.snf_clr_per,"rate":a,"amount":k.amount,"posting_date":format_date(pr_item.posting_date),"shift":pr_item.shift})
 		dlst.append(h)
 		
 	sorted_data = sorted(dlst, key=lambda x: (x["posting_date"], x["shift"].lower() != "morning"))
